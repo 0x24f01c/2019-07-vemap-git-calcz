9fd99e944e67078455c886cf97007e7c55fe37b8
F_S <t3y@keemail.me>
2019-07-26 08:23:28 +0200

    added script to focus input on load

--- CalcZ.php
+++ CalcZ.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Schulung</title>
-	<meta charset="UTF-8">
+    <title>Schulung</title>
+    <meta charset="UTF-8">
     <script
             src="https://code.jquery.com/jquery-3.4.1.min.js"
             integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
@@ -14,75 +14,80 @@
         max-width: 800px;
         text-align: center;
     }
+
     input#submit {
         display: none;
     }
+
     div#ergebnis {
         font-size: 1.5rem;
         text-align: center;
     }
 </style>
 <body>
-<form action="<?php  echo $_SERVER["PHP_SELF"];   ?>" method="post">
+<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 
-	Zahl1<br>
-	<input type="text" name="zahl1"><br>
+    Zahl1<br>
+    <input type="text" name="zahl1"><br>
 
-	Berechnung<br>
-	<select name="rechenart">
-		<option value="addieren">+ addieren</option>
-		<option value="subtrahieren">- subtrahieren</option>
-		<option value="multiplizieren">* multiplizieren</option>
-		<option value="dividieren">/ dividieren</option>
-	</select>
-	<br>
-	Zahl2<br>
-	<input type="text" name="zahl2"><br>
+    Berechnung<br>
+    <select name="rechenart">
+        <option value="addieren">+ addieren</option>
+        <option value="subtrahieren">- subtrahieren</option>
+        <option value="multiplizieren">* multiplizieren</option>
+        <option value="dividieren">/ dividieren</option>
+    </select>
+    <br>
+    Zahl2<br>
+    <input type="text" name="zahl2"><br>
 
-	<input type="submit" value="Absenden"><br>
+    <input type="submit" value="Absenden"><br>
 
 </form>
 
 <?php
-if(isset($_POST["zahl1"]))
-{
-	$zahl1=(float)$_POST["zahl1"];
-	$zahl2=(float)$_POST["zahl2"];
-	$rechenart=$_POST["rechenart"];
+if (isset($_POST["zahl1"])) {
+    $zahl1 = (float)$_POST["zahl1"];
+    $zahl2 = (float)$_POST["zahl2"];
+    $rechenart = $_POST["rechenart"];
 
-	echo "Zahl1: $zahl1";
-	echo "<br>\n";
-	echo "Zahl2: $zahl2";
-	echo "<br>\n";
-	echo $rechenart;
-	echo "<br>\n";
+    echo "Zahl1: $zahl1";
+    echo "<br>\n";
+    echo "Zahl2: $zahl2";
+    echo "<br>\n";
+    echo $rechenart;
+    echo "<br>\n";
 
-	switch($rechenart)
-	{
-		case "addieren": $ergebnis=$zahl1+$zahl2;
-				break;
-		case "subtrahieren": $ergebnis=$zahl1-$zahl2;
-				break;
-		case "multiplizieren": $ergebnis=$zahl1*$zahl2;
-				break;
-		case "dividieren": 
-				if($zahl2==0)
-				{
-					$ergebnis="Keine Divisionen durch null!";
-				}
-				else
-				{
-					$ergebnis=$zahl1/$zahl2;	
-				}
-				
-				break;
-		default: $ergebnis="keine gültige Rechenart";
-	}
+    switch ($rechenart) {
+        case "addieren":
+            $ergebnis = $zahl1 + $zahl2;
+            break;
+        case "subtrahieren":
+            $ergebnis = $zahl1 - $zahl2;
+            break;
+        case "multiplizieren":
+            $ergebnis = $zahl1 * $zahl2;
+            break;
+        case "dividieren":
+            if ($zahl2 == 0) {
+                $ergebnis = "Keine Divisionen durch null!";
+            } else {
+                $ergebnis = $zahl1 / $zahl2;
+            }
 
-	echo "ERGEBNIS:" . $ergebnis;
+            break;
+        default:
+            $ergebnis = "keine gültige Rechenart";
+    }
+
+    echo "ERGEBNIS:" . $ergebnis;
 
 }
 ?>
-
+<script>
+    $(document).ready(function () {
+        $('.berechnung').focus().select();
+    });
+</script>
 </body>
 </html>
